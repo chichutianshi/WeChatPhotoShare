af898f590e97b4a629bc95967731f22ca5a17609
unknown <unknown@example.com>
2019-05-04 17:28:30 +0800

    2019/5/4/17:24
    1122

--- pages/share_page/share.js
+++ pages/share_page/share.js
@@ -13,8 +13,70 @@ Page({
     thirdSessionKey: '',
     introduce: '',
     location: '',
-    photoId: ''
+    photoId: '',
   },
+
+  getlocation:function(e){
+    var that=this
+    wx.chooseLocation({
+      success:function(res) {
+        console.log("授权成功")
+        that.setData({
+          location:res.name
+        })
+      },
+      fail:function(res){
+        console.log("授权失败")
+        wx.getSetting({
+          success:function(res){
+            var statu=res.authSetting;
+            if (!statu['scope.userLocation']) {
+              wx.showModal({
+                title: '是否授权当前位置',
+                content: '需要获取您的地理位置，请确认授权，否则地图功能将无法使用',
+                success: function (tip) {
+                  if (tip.confirm) {
+                    wx.openSetting({
+                      success: function (data) {
+                        if (data.authSetting["scope.userLocation"] === true) {
+                          wx.showToast({
+                            title: '授权成功',
+                            icon:'success',
+                            duration: 1000
+                          })
+                          wx.chooseLocation({
+                            success: function(res) {
+                              that.setData({
+                                location: res.address
+                              })
+                            },
+                          }) 
+                        }else{
+                          wx.showToast({
+                            title: '授权失败',
+                            icon: 'success',
+                            duration: 1000
+                          })
+                        }
+                      }
+                    })
+                  }
+                }
+              })
+            }
+          },
+          fail: function (res) {
+            wx.showToast({
+              title: '调用授权窗口失败',
+              icon: 'success',
+              duration: 1000
+            })
+          }
+        })
+      }
+    })
+  },
+
   // 删除图片
   clearImg: function(e) {
     var nowList = []; //新数据
@@ -89,6 +151,7 @@ Page({
           'thirdSessionKey': that.data.thirdSessionKey,
           'introduce': that.data.introduce,
           'photoId': that.data.photoId,
+          'location':that.data.location,
           'categories': ''
         },
         success: (res) => {
